53c8c0991be18888de99d424b0d39462e52b9924
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-26 00:27:22 +0100

    pseudocode

--- src/index.js
+++ src/index.js
@@ -30,9 +30,19 @@ linkedListOne, linkedListTwo
 
 Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found. 
 8.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem? 
+yes
+
 9.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method? 
+
+sum of two linked list problem
+
 10. Start with a simple example. Could you restate the problem? Could you restate it still differently? 
+see notes
+
 11. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs? 
+
+make sure linked list values are 0 to 9 integers
+
 12. Progress to more complex examples. What is your updated plan here to find a solution?
 13. If you still cannot solve the proposed problem, try to solve first some related problem. Could you imagine a more accessible related problem? A more general problem? A more special problem? An analogous problem? Could you solve a part of the problem? Keep only a part of the data inputs, drop the other part; how far is the unknown then determined, how can it vary? Could you derive something useful from the data? Could you think of other data appropriate to determine the unknown? Could you change the unknown or the data, or both if necessary, so that the new unknown and the new data are nearer to each other? Did you use all the data? Did you use the whole of the data inputs? Have you taken into account all essential notions involved in the problem?
 
@@ -40,6 +50,7 @@ Step Two (Plan). Come up with concrete examples to help you understand the probl
 Step Three (Solve). Carry out your plan of the solution and check each one of your steps in pseudocode.
 14.  Can you see clearly that the step is correct?
 
+see pseudocode below
 
 IV. 	Step Four (Check). Examine the solution obtained and refactor. 
 15.  Can you check your result? 
@@ -50,3 +61,18 @@ IV. 	Step Four (Check). Examine the solution obtained and refactor.
 20.  Can you improve the performance? 
 21.   How have other people solved this problem?
 
+
+function unionLinkedList(linkedListOne, linkedListTwo) {
+  // create new union array and set to empty
+  // set currentOne variable to head for linkedListOne
+  let union = [];
+  let currentOne = head;
+  // make sure the linked list is an integer value between 0 and 9
+  // from the head, iterate through both linked list and find matches and add to new union array
+  if (linkedListOne > 0 && linkedListOne <= 9) {
+    while(currentOne !== null) {
+
+    }
+  }
+  // nreturn new union array
+}
