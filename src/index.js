c89b39f613e017e89f6e009d32a4d050637cac00
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-25 17:06:06 +0100

    poyla step 1 understand

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ Remember the acronym UPS check
 
 Step One (Understand). We have to understand the problem.
 1. What is the unknown?
+
+create intersection list and union list
+
 2. What are the data inputs?
+
+linkedListOne, linkedListTwo
+
 3. Can you restate the problem in your own words?
+
+create intersection list and union list
+
 4. Can the unknown be determined from the data inputs?
+
+yes
+
 5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory? 
+
+sufficient
+
 6. How should I label important pieces of data input that are a part of the problem?
+
+linkedListOne, linkedListTwo
+
+
 7. Draw a figure. Introduce suitable notation.
 
 Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found. 
